191026df47ac0970dc4f2c9c5222509d49bd9374
Raul Castro <raulc27@gmail.com>
2020-05-03 14:16:12 -0300

    incrementando componente pais

--- src/components/Pais.js
+++ src/components/Pais.js
@@ -5,7 +5,7 @@ export default class Pais extends Component {
 
 
     state={
-        Pais:{},
+        Pais:[],
         Mundo:{}
     }
 
@@ -17,7 +17,7 @@ export default class Pais extends Component {
         const responsePais = await api.get(`/v2/countries/${id}`);
         const responseMundo = await api.get('/v2/all');
 
-        console.log(responsePais.data);
+        //console.log(responsePais.data);
 
         this.setState({Pais: responsePais.data});
         this.setState({Mundo: responseMundo.data});
@@ -30,22 +30,86 @@ export default class Pais extends Component {
 
     render(){
 
-        const {informacao}=this.state;
+        const {Pais, Mundo}=this.state;
 
        
         return(
          
+         
+        
+          <>
           
-            
-          <div>
-            
-                <p>Informações </p>
-                <p>{this.state.Pais.country}</p>
-                <p>{this.state.Pais.cases}</p>
-                <p>{this.state.Mundo.cases}</p>
-
-
+          <div class="jumbotron jumbotron-fluid" >
+              <div class="container">
+                
+                <h1 class="display-4">Informações de "{Pais.country}" </h1>
+                <p class="lead">Comparando dados COVID-19 de "{Pais.country}" com o mundo.</p>
+                <p>Esta <b>app</b> consulta uma api, e muitos dados vem em formato internacional.</p>
+              </div>
           </div>
+            
+
+          <div class="card-deck">
+              <div class="card">
+              {/*<img class="card-img-top" src='' alt='Dados Mundiais' /> */}
+                <div class="card-body">
+                  <h5 class="card-title">{Pais.country}</h5>
+                  <p class="card-text"></p>
+                
+                  <ul class="list-group list-group-flush">
+                  <li class="list-group-item"></li>
+                  <li class="list-group-item"><b>Diagnosticados:</b> {Pais.cases}</li>
+                  <li class="list-group-item"><b>Mortes:</b> {Pais.deaths}</li>
+                  <li class="list-group-item"><b>Recuperados:</b> {Pais.recovered}</li>
+                  <li class="list-group-item"><b>Infectados no momento:</b> {Pais.active}</li>
+                  <li class="list-group-item"><b>Estado Crítico:</b> {Pais.critical}</li>
+              </ul>
+                  
+                  <p class="card-text"><small class="text-muted">Update de 10 em 10min</small></p>
+                </div>
+            
+            </div>
+
+            <div class="card" >
+            {/* <img class="card-img-top" src='' alt='Dados Mundiais' /> */}
+            <div class="card-body">
+              <h5 class="card-title">Mundo</h5>
+              <p class="card-text">
+              </p>
+              <p class="card-text"></p>
+                         
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item"></li>
+                <li class="list-group-item"><b>Diagnosticados:</b> {Mundo.cases}</li>
+                <li class="list-group-item"><b>Mortes:</b> {Mundo.deaths}</li>
+                <li class="list-group-item"><b>Recuperados:</b> {Mundo.recovered}</li>
+                <li class="list-group-item"><b>Infectados no Momento:</b> {Mundo.active}</li>
+                <li class="list-group-item"><b>Estado Cŕitico:</b> {Mundo.critical}</li>
+            </ul>
+
+
+
+              <p class="card-text"><small class="text-muted">Update de 10 em 10min</small></p>
+            </div>
+        
+        </div>
+          </div>
+          
+           
+
+
+          
+
+
+
+          </>
+
+
+
+          
+
+         
+          
    
          
         );
