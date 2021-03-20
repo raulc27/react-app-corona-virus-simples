0004edcdfa4b3178c2468d072fda4ef3ee7ec65d
Raul Castro <raulc27@gmail.com>
2021-03-20 14:26:48 -0300

    ultimas modificacoes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,60 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Button, Card, Nav, Navbar } from 'react-bootstrap';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { AiOutlineTwitter } from 'react-icons/ai';
+import { GoMarkGithub } from 'react-icons/go';
 import { Link } from 'react-router-dom';
 
 import Logo from '../Assets/logo192.png';
+import raul from '../Assets/raul-github.jpeg';
 
 const NavBar = () => {
   return (
 
 
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" className="justify-content-between">
     <Navbar.Brand ><Link to={`/`}> <img src={Logo} width="50px" />  </Link></Navbar.Brand>
 
     <Nav >
-    <NavDropdown title="Links Úteis" id="collasible-nav-dropdown" className="mr-sm-2">
+    <NavDropdown title="Links Úteis" id="collasible-nav-dropdown" className="mr-sm-2 ">
         <NavDropdown.Item href="https://www.bing.com/covid" target="_blank">Rastreador Bing</NavDropdown.Item>
         <NavDropdown.Item href="https://experience.arcgis.com/experience/38efc69787a346959c931568bd9e2cc4"  target="_blank">Casos por Bairro/Rio de Janeiro</NavDropdown.Item>
         <NavDropdown.Item href="https://www.worldometers.info/coronavirus/"  target="_blank">WorldOMeters</NavDropdown.Item>
-        <NavDropdown.Divider />
+        
         <NavDropdown.Item href="http://covid19br.org/"  target="_blank">Painel UFBA</NavDropdown.Item>
-        <NavDropdown.Item  href="https://github.com/raulc27/react-app-corona-virus-simples" target="_blank">GitHub desta App</NavDropdown.Item>
         <NavDropdown.Divider />
+        <NavDropdown.Item  href="https://github.com/raulc27/react-app-corona-virus-simples" target="_blank">GitHub desta App</NavDropdown.Item>
+       
 
       </NavDropdown>
     </Nav>
 
 
-    <Nav>
-          
+    <Nav  >
+
+    <NavDropdown title="Quem desenvolve" id="nav-dropdown">
+    
+          <Card style={{ width: '18rem' }} className="pr-1">
+        <Card.Img variant="top" src={raul}  />
+        <Card.Body>
+          <Card.Title>Raul Castro</Card.Title>
+          <Card.Text>
+           
+          </Card.Text>
+          <a href="https://twitter.com/raulc27" target="_blank"> <Button variant="outline-info"><AiOutlineTwitter /> Twitter</Button></a>
+          &nbsp;&nbsp;&nbsp;
+          <a href="https://github.com/raulc27" target="_blank"><Button variant="outline-dark"><GoMarkGithub /> Github</Button></a>
+        </Card.Body>
+      </Card>
+
+
+
+      </NavDropdown>
     </Nav>
+
+    <Nav></Nav>
    
  
   </Navbar>
