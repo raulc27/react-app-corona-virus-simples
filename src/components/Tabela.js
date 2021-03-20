44413f3a724fdee9a7ff9457b81441460284bb89
Raul Castro <raulc27@gmail.com>
2021-03-19 22:33:22 -0300

    ultimas alteracoes

--- src/components/Tabela.js
+++ src/components/Tabela.js
@@ -1,50 +1,12 @@
-import React,{Component} from 'react';
-<<<<<<< HEAD
-import tabela from './tabela';
-=======
+import '../buttonStyles.css';
+
+import React, { Component } from 'react';
+
 import api from '../services/api';
 import Tab from './tab';
->>>>>>> 0544d743c537f96ed13b333d3d3634da9fa6fe29
 
 
 
-
-<<<<<<< HEAD
-export default class Tabela extends Component{
-
-
-    state={
-        coronas:[]
-    }
-  
-    componentDidMount(){
-      fetch('https://corona.lmao.ninja/v2/countries')
-      .then(res=>res.json())
-      .then((data)=>{
-        this.setState({coronas:data})
-      })
-      .catch(console.log)
-    }
-
-
-
-
-render(){
-
-    return(
-
-        <tabela  coronas={this.state.coronas}   />
-
-    );
-
-}
-
-
-
-
-}
-=======
-import "../buttonStyles.css";
 
 class Tabela extends Component{
 
@@ -90,4 +52,3 @@ async componentDidMount(){
 }
 
 export default Tabela;
->>>>>>> 0544d743c537f96ed13b333d3d3634da9fa6fe29
--- src/components/corona.js
+++ src/components/corona.js
@@ -8,7 +8,7 @@ const Corona = ({coronas})=>{
 
     function(a,b)
     {
-        return b.active-a.active;
+        return b.cases-a.cases;
     }
 
   )
@@ -19,7 +19,7 @@ const Corona = ({coronas})=>{
               <div >
             <center><h1>Casos COVID-19 no Mundo</h1>
             <p>clique na bandeira do país para um resumo</p>
-            <p>Para visualizar por óbitos, <Link to={`/Tabela`} > clique aqui</Link></p>
+            <p>Para visualizar em tabela, <Link to={`/Tabela`} > clique aqui</Link></p>
             </center>
           
             <div class="container">
@@ -33,33 +33,25 @@ const Corona = ({coronas})=>{
                 .map((corona)=>(
                                      
                    
-                    <div class="card mb-5" style={{width:'21em'}} >
+                    <div class="card" style={{width:'21em'}} >
  
                     <Link to={`/Pais/${corona.country}`} > <img src={corona.countryInfo.flag} class="card-img-top" />  </Link>
                    
                         <div class="card-body"  >
-                        <h3 class="card-title">{corona.country}</h3>
-                        <small><b>Infectados no momento: </b>{Intl.NumberFormat("pt-BR",{style:'decimal'}).format(corona.active)}</small>
-                       
+                        <h5 class="card-title">{corona.country}</h5>
                         <p class="card-text"></p>
+                        </div>
 
-
-                        <div class="card-body"  >
-                       
-                       
                             <ul class="list-group list-group-flush">
-
-                          
-
                             <li class="list-group-item"><b>Total Diagnosticados: </b>{Intl.NumberFormat("pt-BR",{style:'decimal'}).format(corona.cases)}</li>
-                           
+                            <li class="list-group-item"><b>Infectados agora: </b>{Intl.NumberFormat("pt-BR",{style:'decimal'}).format(corona.active)}</li>
                             <li class="list-group-item"><b>Taxa Letalidade: </b>{Intl.NumberFormat("pt-BR",{style:'percent'}).format(corona.deaths/corona.cases)}</li>
                             <li class="list-group-item"><b>Taxa Recuperação: </b>{Intl.NumberFormat("pt-BR",{style:'percent'}).format(corona.recovered/corona.cases)}</li>
                             <li class="list-group-item"><b>Qtde testes realizados: </b>{Intl.NumberFormat("pt-BR",{style:'decimal'}).format(corona.tests)}</li>
                             <li class="list-group-item"><b>Mortes hoje: </b>{Intl.NumberFormat("pt-BR",{style:'decimal'}).format(corona.todayDeaths)}</li>
                             </ul>
 
-                            </div>                     
+                     
 
                         <div class="card-footer">
                         <small class="text-muted">Atualização de 10 em 10 minutos</small>
@@ -75,7 +67,7 @@ const Corona = ({coronas})=>{
                                 {corona.todayDeaths}
                                 {corona.critical}
                                   
-                */}     
+                */}
                        </div>
                 ))}
 
