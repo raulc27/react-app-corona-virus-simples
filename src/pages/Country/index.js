65168efc878c87c80510ec484e9e365bca5ec922
Raul Castro <raulc27@gmail.com>
2022-02-23 08:44:00 -0300

    cleaning files, create new page Country for Pais.js component update

---
+++ src/pages/Country/index.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const Country = () => {
+
+    const [Pais, setPais] = useState([]);
+    const [Mundo, setMundo] = useState([]);
+}
+
+export default Country;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import NotFound from './components/404';
 import NavBar from './components/NavBar';
-import Pais from './components/Pais';
-import Tabela from './components/Tabela';
 import Countries from './pages/Countries';
+import Pais from './pages/Country';
 
 
 
@@ -17,7 +15,6 @@ const Routes = () => (
         
             <Route exact path="/" component={Countries} />
             <Route path="/Pais/:id" component={Pais} />
-            <Route path="/Tabela" component={Tabela} />
             <Route component={NotFound} />
         </Switch>
     </BrowserRouter>
