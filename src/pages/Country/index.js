9ccbc5cb90cb2281616e7276ca847b4b4b128849
Raul Castro <raulc27@gmail.com>
2022-02-23 10:18:50 -0300

    implementing  Country.js

--- src/pages/Country/index.js
+++ src/pages/Country/index.js
@@ -1,9 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react';
 
+import api from '../../services/api';
+
 const Country = () => {
 
     const [Pais, setPais] = useState([]);
     const [Mundo, setMundo] = useState([]);
+    const {id} = props.match.params;
+
+    async function getCountryData(){
+    try{
+        const {data:{data=[]}} = await api.get(`/v2/countries/${id}`);
+        return data;
+    }
+    catch(_e){
+        console.error("Error: ", _e);
+    }
+    }
+
+    async function getWorldData(){
+        try{
+          const {data:{data=[]}} = await api.get(`/v2/all`);
+          return data;
+        }
+        catch(_e){
+            console.error('Error: ',_e);
+        }
+    }
 }
 
 export default Country;
