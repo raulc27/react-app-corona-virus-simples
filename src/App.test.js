a9e04382ddc5b90966dc407cc8e5d217f8c18194
Raul Castro <raulc27@gmail.com>
2022-03-09 01:22:32 -0300

    deleting some old files

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-/*test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});*/
